6b6a9bd640087b983fa68279df5bbb8292261170
N0ot-No0t <sustaccosmin544@hotmail.ca>
2020-05-21 23:51:06 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NzEzMTgzODg2NDMxMjIzOTM5.XsccXA.eBgVtnrUyob3iNg5qnQA4ULQh18';
+
+const prefix1 = "&";
+const prefix2 = "*";
+
+bot.on("ready", () => {
+
+    console.log("SpongeBot is online.");
+
+});
+
+bot.on("message", msg => {
+
+    //let args = msg.content.substring(prefix.length).split(" ");
+
+
+    if (msg.content.charAt(0) === prefix1) {
+
+        croppedMsg = msg.content.substring(1);
+
+        var msgArray = new Array(croppedMsg.length);
+
+        for (var i = 0; i < croppedMsg.length; i++) {
+
+            if (Math.random() >= 0.5) {
+
+                msgArray[i] = croppedMsg.toString().charAt(i).toUpperCase();
+
+            } else {
+                msgArray[i] = croppedMsg.toString().charAt(i);
+            }
+
+
+        }
+
+        var outputMsg = msgArray.join('');
+
+        msg.delete();
+
+        msg.reply(outputMsg.toString());
+    } else if (msg.content.charAt(0) === prefix2) {
+
+        croppedMsgReverse = msg.content.substring(1).toLowerCase();
+
+        msg.delete();
+
+        msg.reply(croppedMsgReverse);
+
+    }
+
+
+});
+
+bot.login(token);
